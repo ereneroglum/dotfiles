4bc03649601beeb68c5da81ec18aa49b85eafe63
Eren Eroğlu <108634315+eroglueren@users.noreply.github.com>
2022-08-03 22:58:09 +0300

    Added home-manager check for install script.

--- install-home.sh
+++ install-home.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if ! command -v home-manager &> /dev/null
+then
+    echo "home-manager could not be found. Please install home-manager first."
+    exit 1
+fi
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 NIXPKGS_ALLOW_UNFREE=1 home-manager switch -f "${SCRIPT_DIR}/home.nix"
