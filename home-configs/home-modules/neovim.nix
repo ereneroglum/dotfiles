a7693c0069e336e3ce27c59ab8a5882bd5363e3c
Eren Eroğlu <108634315+eroglueren@users.noreply.github.com>
2022-08-13 00:17:41 +0300

    Removed null-ls.

--- home-configs/home-modules/neovim.nix
+++ home-configs/home-modules/neovim.nix
@@ -137,26 +137,6 @@
           after = { 'impatient.nvim' }
         })
 
-        -- Null-ls
-        use({
-          'jose-elias-alvarez/null-ls.nvim',
-          after = { 'impatient.nvim' },
-          config = function()
-            local null_ls = require('null-ls')
-            local sources = {
-              null_ls.builtins.diagnostics.cppcheck,
-              null_ls.builtins.diagnostics.deadnix,
-              null_ls.builtins.diagnostics.flake8,
-              null_ls.builtins.formatting.autopep8,
-              null_ls.builtins.formatting.clang_format,
-              null_ls.builtins.formatting.gofmt,
-              null_ls.builtins.formatting.nixfmt,
-              null_ls.builtins.formatting.rustfmt
-            }
-            null_ls.setup({ sources = sources })
-          end
-        })
-
         -- Plenary
         use({
           'nvim-lua/plenary.nvim',
