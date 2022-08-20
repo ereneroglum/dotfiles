366ff43d8700cecc22485c7e12da798ebb252514
Eren Eroğlu <108634315+eroglueren@users.noreply.github.com>
2022-08-20 21:51:02 +0300

    Fixed nvim-tree && project.nvim integration.

--- home-configs/home-modules/neovim.nix
+++ home-configs/home-modules/neovim.nix
@@ -94,10 +94,10 @@
           config = function()
             require('nvim-tree').setup({
               respect_buf_cwd = true,
-              update_cwd = true,
+              sync_root_with_cwd = true,
               update_focused_file = {
                 enable = true,
-                update_cwd = true
+                update_root = true
               }
             })
           end
