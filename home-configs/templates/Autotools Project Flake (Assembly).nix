640ec88014bab6973cd76974e97bde22e1c60959
Eren Eroğlu <108634315+eroglueren@users.noreply.github.com>
2022-12-10 14:22:21 +0300

    Added python and assembly flakes.

---
+++ home-configs/templates/Autotools Project Flake (Assembly).nix
@@ -0,0 +1,34 @@
+{
+  description = "Flake to build @packagename@";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          @packagename@ = pkgs.stdenv.mkDerivation {
+            pname = "@packagename@";
+            version = "@packageversion@";
+            nativeBuildInputs = with pkgs; [ autoreconfHook nasm yasm ];
+            src = ./.;
+          };
+        };
+
+        defaultPackage = packages.@packagename@;
+
+        devShell = pkgs.mkShell {
+          shellHook = ''
+            SHELL=${pkgs.bashInteractive}/bin/bash
+          '';
+          packages = with pkgs; [ bashInteractive ];
+          inputsFrom = [ packages.@packagename@ ];
+        };
+      }
+    );
+}
---
+++ home-configs/templates/Python Project Flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Flake to build @packagename@";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          @packagename@ = pkgs.python3Packages.buildPythonPackage {
+            pname = "@packagename@";
+            version = "@packageversion@";
+            src = ./.;
+          };
+        };
+
+        defaultPackage = packages.@packagename@;
+
+        devShell = pkgs.mkShell {
+          shellHook = ''
+            SHELL=${pkgs.bashInteractive}/bin/bash
+          '';
+          packages = with pkgs; [
+            bashInteractive
+            (python3.withPackages (ps: with ps; [
+              python-lsp-server
+            ]))
+          ];
+          inputsFrom = [ packages.@packagename@ ];
+        };
+      }
+    );
+}
