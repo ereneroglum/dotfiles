49506a1edf8d395137dbf7e9de8659f02214c72f
Eren Eroğlu <108634315+ereneroglum@users.noreply.github.com>
2022-12-17 20:43:52 +0300

    Added tools for document-processing.

--- home-configs/home-modules/document-processing.nix
+++ home-configs/home-modules/document-processing.nix
@@ -2,12 +2,17 @@
 
 {
   home.packages = with pkgs; [
+    # ghostscript # included with texlive
+    asciidoc-full
+    asciidoctor
+    dblatexFull
     doxygen
     gnuplot_qt
     groff
     pandoc
     plantuml
     sphinx
+    texinfo
     texlive.combined.scheme-full
   ];
 }
