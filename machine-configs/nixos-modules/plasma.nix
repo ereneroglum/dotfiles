30fca1425db6f8c0ad570f30c4beb3276d86c7ef
Eren Eroğlu <108634315+ereneroglum@users.noreply.github.com>
2022-12-23 17:28:43 +0300

    Added bismuth to plasma.

--- machine-configs/nixos-modules/plasma.nix
+++ machine-configs/nixos-modules/plasma.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [ libsForQt5.bismuth ];
+
   services.xserver = {
     displayManager.sddm.enable = true;
     desktopManager.plasma5.enable = true;
