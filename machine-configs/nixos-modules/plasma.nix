a47c5638590644ff945db92c496ca003d36f9916
Eren Eroğlu <108634315+ereneroglum@users.noreply.github.com>
2022-12-26 00:39:22 +0300

    Revert "Added bismuth to plasma."
    This reverts commit 30fca1425db6f8c0ad570f30c4beb3276d86c7ef.

--- machine-configs/nixos-modules/plasma.nix
+++ machine-configs/nixos-modules/plasma.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ libsForQt5.bismuth ];
-
   services.xserver = {
     displayManager.sddm.enable = true;
     desktopManager.plasma5.enable = true;
